f22a59c7d03b699882f528c6ef2484fad470e81e
Bharath Kumar Shivakumar <bskumar1@outlook.com>
2021-05-05 09:15:31 -0400

    Added Details Modal and Open outage click events
    Certain parts of the code will facilitate the click events from the Server script, the open outages will trigger a Business Application Service Timeline widget

--- App_Avail_Domain_Filter/Client Script.js
+++ App_Avail_Domain_Filter/Client Script.js
@@ -8,11 +8,71 @@ function($scope, $rootScope, spUtil, spModal) {
 		}
 		$scope.data.dates[x] = dateObj;
 	}
-	
-	var c = this;
 
-	$scope.showAppStatus = function(domValue){
+	var c = this;
+	$scope.showAppStatus = function(domValue,domName){
+		c.data.domainName = domName;
 		c.data.domainValue = domValue;
 		c.server.update();
+		console.log($scope.data.cppApps);
 	}
+
+	c.doClick = function($event, outage) {
+		if (outage[0] == "detailsModal") {
+			c.detailsModal($event, outage[2], outage[3]);
+		} else if (outage[0] == "openOutage") {
+			c.openOutage($event, outage[1], outage[2], outage[3])
+		}
+	}
+
+	c.detailsModal = function($event, $service, $name){
+		var serviceId = $service;
+		spModal.open({
+			widget: 'business_application_service_timeline',
+			widgetInput: {
+				service:serviceId
+			},
+			size: 'lg',
+			title: $name,
+			buttons: []
+		})
+	}
+
+	c.openOutage = function($event, type, $outage, $name){
+		var icon = "";
+		var text = "";
+
+		if (type == "outage") {
+			icon = "minus";
+			text = "danger";
+		} else if (type == "performance_degradation") {
+			icon = "exclamation";
+			text = "warning";
+		} else if (type == "planned") {
+			icon = "info";
+			text = "primary";
+		} else if (type == "partial") {
+			icon = "times";
+			text = "circle";
+		} else if (type == "multiple") {
+			icon = "plus";
+			text = "circle";
+		} else if (!type) {
+			icon = "question";
+			text = "circle";
+		}
+
+		var outageId = $outage;
+		spModal.open({
+			widget: 'business_app_outage_details',
+			widgetInput: {
+				outage:outageId
+			},
+			title: '<h3 style="margin-top: 10px;"><i class="fa fa-' + icon + '-circle text-' + text + '"></i> ' + $name + '</h3>',
+			buttons: []
+		})
+	}
+
+	$scope.$on("shown.bs.modal", function(e) {})
+
 }
