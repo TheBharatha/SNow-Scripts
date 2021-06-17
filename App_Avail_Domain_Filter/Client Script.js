3db236d9058e22b6d3dc6ad666fa27048c3393d5
Bharath Kumar Shivakumar <bskumar1@outlook.com>
2021-06-17 09:59:26 -0400

    New functions for incomplete outages
    Wrote a new function for incomplete outage records, to show them on the report

--- App_Avail_Domain_Filter/Client Script.js
+++ App_Avail_Domain_Filter/Client Script.js
@@ -1,4 +1,4 @@
-function($scope, $rootScope, spUtil, spModal) {
+function($scope, $rootScope, spUtil, spModal, $window) {
 
 	for (var x in $scope.data.dates) {
 		var dateStr = moment($scope.data.dates[x]).format('ll').split(',')[0];
@@ -24,6 +24,25 @@ function($scope, $rootScope, spUtil, spModal) {
 		}
 	}
 
+	c.blankOuts = function() {
+		if (data.bCount > 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	c.showOuts = function(bOuts) {
+		var urlStrO = 'https://comerica.service-now.com/nav_to.do?uri=cmdb_ci_outage_list.do?sysparm_query=';
+		for (var sID=0; sID< bOuts.length; sID++) {
+			urlStrO = urlStrO.concat("sys_id=").concat(bOuts[sID]);
+			if (sID !== bOuts.length-1) {
+				urlStrO = urlStrO.concat("^OR");
+			}
+		}
+		$window.open(urlStrO, '_blank');
+	}
+
 	c.detailsModal = function($event, $service, $name){
 		var serviceId = $service;
 		spModal.open({
@@ -37,15 +56,32 @@ function($scope, $rootScope, spUtil, spModal) {
 		})
 	}
 
+	//To open the Incident on a new tab
+	c.openInc = function(incsysid) {
+		$window.open('https://comerica.service-now.com/x_fru_inc_incident.do?sys_id='+incsysid, '_blank');
+	}
+
+	//To open the group of Incidents on new tab
+	c.multInc = function(manyInc) {
+		var urlStr = 'https://comerica.service-now.com/nav_to.do?uri=x_fru_inc_incident_list.do?sysparm_query=';
+		for (var i=0; i< manyInc.length; i++) {
+			urlStr = urlStr.concat("number=").concat(manyInc[i]);
+			if (i !== manyInc.length-1) {
+				urlStr = urlStr.concat("^OR");
+			}
+		}
+		$window.open(urlStr, '_blank');
+	}
+
 	c.openOutage = function($event, type, $outage, $name){
 		var icon = "";
 		var text = "";
 
 		if (type == "outage") {
-			icon = "minus";
+			icon = "exclamation";
 			text = "danger";
 		} else if (type == "performance_degradation") {
-			icon = "exclamation";
+			icon = "minus";
 			text = "warning";
 		} else if (type == "planned") {
 			icon = "info";
